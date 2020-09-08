619f4a174455522f73c5c7eaf1443e09bf2cac59
Vinicius Pedrinho <vrpedrinho@gmail.com>
2020-09-08 17:19:11 -0300

    sei lá :P

--- python/Challenge.py
+++ python/Challenge.py
@@ -8,28 +8,24 @@ sixth_value = 'sixth'
 
 # First challenge
 first_value=first_value.strip()
-second_value=second_value.replace('-  ','')
-second_value=second_value.replace('  -','')
-second_value=second_value.capitalize()
-third_value=third_value.replace(' ','')
-third_value=third_value.capitalize()
-third_value=third_value.replace('-',' ')
-
-print(f'{first_value:>22}')
-print(second_value)
-print(f'{third_value:>30}')
 
 print(fourth_value,fifth_value,sixth_value, sep='#')
 
 # Second challenge
+second_value=second_value.replace('-  ','')
+second_value=second_value.replace('  -','')
+second_value=second_value.capitalize()
 
 
 # Third challenge
+third_value=third_value.replace(' ','')
+third_value=third_value.capitalize()
+third_value=third_value.replace('-',' ')
 
-"""
-print(f'.{first_value:>26}.')
-print(f'.{second_value:<5}.')
-print(f'.{third_value:<5}.')"""
+
+print(f'{first_value:>22}')
+print(second_value)
+print(f'{third_value:>30}')
 
 # Fourth challenge - use only the print() function (no f-strings)
 
